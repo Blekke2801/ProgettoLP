5f2acd88524312fea11a76f72ff8b3fde90e9a5b
Blekke <super.nuele01@gmail.com>
2022-11-23 11:25:28 +0100

    forse funge

--- code/libraries/ParserProlog.pl
+++ code/libraries/ParserProlog.pl
@@ -1,12 +1,21 @@
+
+%%% jsonparse/2 jsonparse(JSONString, Object).
+%%% prima funzione, vera quando la stringa può essere scomposta in stringhe numeri o termini composti
+jsonparse("", Object).
+jsonparse(JSONString, Object) :- 
+    string_split(JSONString, " ", X), jsonparse(X, Object).
+jsonparse(X, Object) :- 
+    jsonparse(["{" | Members | "}"], Object);
+    jsonparse(["[" | Elements | "]"], Object).
+
 Object = jsonobj(Members) ; jsonarray(Elements).
 Members = [] ; [Pair | MoreMembers].
-Pair = [Attribute | ": " | Value].
+Pair = [Attribute | ":" | Value].
 Attribute = string(X).
 Value = string(X) ; Object ; number(X).
 
-jsonparse/2 jsonparse(JSONString, Object).
-%%% prima funzione, vera quando la stringa può essere scomposta in stringhe numeri o termini composti
-jsonparse(JSONString,Object) :- 
+Elements = [] ; [Value | "," | MoreElements].
+
 
 
 jsonaccess/3 jsonaccess(Jsonobj, Fields, Result).
@@ -14,7 +23,11 @@ jsonaccess/3 jsonaccess(Jsonobj, Fields, Result).
 %%% (una lista) a partire da Jsonobj. Un campo rappresentato da N (con N un numero maggiore o
 %%% uguale a 0) corrisponde a un indice di un array JSON.
 
-jsonaccess(Jsonobj, Fields, Result) :- jsonparse(Jsonobj,Object), jsonaccess(Object, [], Result).
-jsonaccess(jsonobj(Members), Fields, Result) :- jsonaccess([[Attribute | ": " | Value] | MoreMembers], [Field | MoreFields], Result), Attribute = Field.
-jsonaccess([[Attribute | ": " | Value] | MoreMembers], [Field | MoreFields], Result), Attribute = Field :- jsonaccess([MoreMembers], [MoreFields], [Value]).
+jsonaccess(Object, [], Result).
+
+jsonaccess(jsonobj(Members), Fields, Result) :- 
+    jsonaccess([[Attribute | ": " | Value] | MoreMembers], [Field | MoreFields], Result),
+    Attribute = Field.
+jsonaccess([[Attribute | ": " | Value] | MoreMembers], [Field | MoreFields], Result), Attribute = Field :- 
+    jsonaccess([MoreMembers], [MoreFields], [Value]).
 
--- code/page/script.js
+++ code/page/script.js
@@ -4,12 +4,13 @@ function parse() {
     stringaJSON = document.getElementById("areaText").value;
     output = document.getElementById("areaRis");
     var session = pl.create();
-    session.consult("../libraries/ParseProlog.pl" {
-        success: function() {},
+    session.consult("../libraries/ParserProlog.pl", {
+        success: function() {
+            session.query("jsonparse(" + stringaJSON + ", Object).", {
+            success: function(goal) {output.value = "Stringa JSON inserita con successo";},
+            error: function(err) {output.value = "Stringa JSON inserita con sintassi errata impossibile tradurla";}
+        });},
         error: function(err) {alert("Errore nel caricamento della libreria")}
     });
-    session.query("jsonparse(" + stringaJSON + ", Object).", {
-        success: function(goal) {output.value = "Stringa JSON inserita con successo";},
-        error: function(err) {output.value = "Stringa JSON inserita con sintassi errata impossibile tradurla";}
-    });
+    
 }
