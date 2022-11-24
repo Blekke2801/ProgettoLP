0d7459229f551c150f9746428c36ae6752736b92
Blekke <super.nuele01@gmail.com>
2022-11-24 15:34:24 +0100

    de

--- code/libraries/ParserProlog.pl
+++ code/libraries/ParserProlog.pl
@@ -10,7 +10,8 @@ jsonparse/2 jsonparse(JSONString, Object).
 %%% prima funzione, vera quando la stringa può essere scomposta in stringhe numeri o termini composti
 
 
-jsonaccess/3 jsonaccess(Jsonobj, Field, Result).
+jsonaccess/3 jsonaccess(Jsonobj, Fields, Result).
 %%% vero quando Result è recuperabile seguendo la catena di campi presenti in Fields
 %%% (una lista) a partire da Jsonobj. Un campo rappresentato da N (con N un numero maggiore o
 %%% uguale a 0) corrisponde a un indice di un array JSON.
+
