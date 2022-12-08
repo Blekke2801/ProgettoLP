84b28ff3f3dda92f12f6fc8fbf8438ef233578ab
Blekke <83006362+Blekke2801@users.noreply.github.com>
2022-12-08 17:12:55 +0100

    da

--- code/libraries/ParserProlog.pl
+++ code/libraries/ParserProlog.pl
@@ -1,77 +1,125 @@
 %%% jsonparse/2 jsonparse(JSONString, Object).
 %%% prima funzione, vera quando la stringa può essere scomposta in stringhe numeri o termini composti
 
+%%% riconoscimento di jsonobj
+
+jsonobj(Members) :- 
+    length(Members,0);
+    (
+        Members is [Pair, ','| MoreMembers], 
+        Pair is [Attribute, ':', Value], 
+        string(Attribute), 
+        (
+            string(Value); 
+            number(Value); 
+            Value is jsonobj(Members)
+        )
+    ).
+
+%%%riconoscimento di jsonarray
+jsonarray(Elements) :-
+    length(Elements, 0); 
+    (
+        Elements is [Value | MoreValues],
+        (
+            string(Value); 
+            Value is jsonobj(Members); 
+            number(Value) 
+        )
+    ).
+
+
 jsonparse("", []).
 
 jsonparse(JSONString, Object) :- 
     string(JSONString),
-    split_string(JSONString, "\s","\s", X), 
-    jsonparse(X, Object),
-    delete("\n", X, Xt).
+    split_string(JSONString, "\s\n","\s\n", X),
+    jsonparse(Xt, Object).
 
+%%% controllo delle parentesi all'inizio e alla fine
+jsonparse(Xt, Object) :-  
+    (
+        nth0(0,Xt,'{'), 
+        last(Xt, '}'), 
+        Xt is [A | Other],
+        jsonparse([A | Other], Object)
+    );
+    (
+        nth0(0,Xt,'['), 
+        last(Xt, ']'), 
+        Xt is [B | Other],
+        jsonparse([B | Other], Object)
+    ).
 
-jsonparse(Xt, Object) :- 
-    Xo is "{",
-    Xc is "}",
-    Yo is "[",
-    Yc is "]",   
-    (Xt is [Xo | Members], last(Xt, Xc), jsonparse([Xo | Members], Object));
-    (Xt is [Yo | Elements], last(Xt, Yc), jsonparse([Yo | Elements], Object)).
-
-jsonparse([Xo | Members], Object) :- 
-    selectchk([Xo | Members], Xo, New),
-    reverse(New, R_new),
-    selectchk(R_new, Xc, New2),
-    reverse(New2, Members),
-    (Members is [Pair | MoreMembers], jsonparse([[]], Object); 
-    jsonparse([Pair | MoreMembers], [])).
+jsonparse([A | Other], Object) :- 
+    (
+        jsonobj([A | Other]);
+        (
+            selectchk([A | Other], A, New),
+            reverse(New, R_new), 
+            selectchk(R_new, '}',  New2), 
+            reverse(New2, Members), 
+            jsonobj(Members)
+        )
+    ),
+    jsonparse(jsonobj(Members), Object). 
 
 jsonparse([], Object) :- 
-    jsonparse([], [Xo, Xc]).
+    jsonparse([], []).
 
-jsonparse([Pair | MoreMembers], Object) :- 
-    string_chars(Punti, ":"),
-    Pair is [Attribute , Punti , Value],
-    jsonparse([[Attribute , Punti , Value] | MoreMembers], Object).
+jsonparse(jsonobj(Members),Object) :- 
+    jsonparse([[Attribute, ':', Value] | MoreMembers], Object).
 
-jsonparse([[Attribute, Punti, Value] | MoreMembers], Object) :-
+jsonparse([[Attribute, ':', Value] | MoreMembers], Object) :-
     string(Attribute),
-    (string(Value); jsonobj(Value); number(Value)),
-    jsonparse([MoreMembers], [[Attribute, Punti, Value]]).
+    (
+        string(Value); 
+        jsonobj(Value); 
+        number(Value)
+    ),
+    append(Object, [[Attribute, ':', Value]], NewObject),
+    jsonparse(MoreMembers, NewObject).
 
-jsonparse([MoreMembers], [Xo, [[Attribute, Punti, Value]], Xc]) :-
-    MoreMembers is [Pair | MoreMembers],
-    jsonparse([Pair | MoreMembers], [Xo, [[Attribute, Punti, Value] | Other], Xc]).
-
-jsonparse([Pair | MoreMembers], [Xo, [[Attribute, Punti, Value] | Other], Xc]) :-
-    Object is [Xo, [[Attribute, Punti, Value] | Other], Xc],
+jsonparse(MoreMembers, NewObject) :-
+    MoreMembers is jsonobj(Members),
+    Object is NewObject,
     jsonparse([Pair | MoreMembers], Object).
 
-jsonparse([Yo | Elements], Object) :- 
-    selectchk([Yo | Elements], Yo, New),
-    reverse(New, R_new),
-    selectchk(R_new, Yc,  New2),
-    reverse(New2, Elements),
-    delete(",", X, Elementst).
-    Elementst is [Value | MoreValues],
-    jsonparse([Value | MoreValues], Object); 
-    jsonparse([[]], Object).
-
-jsonparse([Value | MoreValues], Object) :-
-    (string(Value); Value is Object; number(Value)),
-    jsonparse([MoreValues], [[Value | Other]]).
-
-jsonparse([MoreValues], [Value | Other]) :-
-    Object is [Value | Other],
+jsonparse([B | Other], Object) :- 
+    (
+        (   
+            jsonarray([B | Other]),
+            Elements is [B | Other] 
+        );
+        (
+            selectchk([B | Other], B, New),
+            reverse(New, R_new), 
+            selectchk(R_new, ']',  New2), 
+            reverse(New2, Elements), 
+            jsonarray(Elements)
+        )
+    ),
+    jsonparse(jsonarray(Elements), Object).
+    
+jsonparse(jsonarray(Elements),Object) :- 
     jsonparse([Value | MoreValues], Object).
 
-jsonparse([[]], Object) :- 
-    jsonparse([], [Yo, Yc]).
+jsonparse([Value | MoreValues], Object) :-
+    (
+        string(Value); 
+        Value is Object; 
+        number(Value)
+    ),
+    append(Object, [Value], NewObject),
+    jsonparse(MoreValues, NewObject).
 
-jsonparse([], [[Attribute, Punti, Value] | MoreMembers]).
-jsonparse([], [[[Value | Virgola] | MoreValues]]).
+jsonparse(MoreValues, NewObject) :-
+    MoreValues is [Value | MoreValues],
+    Object is NewObject,
+    jsonparse([Value | MoreValues], Object).
+
+jsonparse([], NewObject).
 jsonparse([], []).
-jsonparse([], [[[Value, Virgola] | MoreValues]]).
 
 
 
@@ -93,12 +141,12 @@ jsonaccess(Jsonobj,Fields,Result) :-
 
 jsonaccess(Object, [Field | MoreFields], Result) :-
     Object is [Pair | MoreMembers],
-    Pair is [Attribute , Punti , Value],
-    jsonaccess([[Attribute , Punti , Value] | MoreMembers], [Field | MoreFields], Result).
+    Pair is [Attribute , ':' , Value],
+    jsonaccess([[Attribute , ':' , Value] | MoreMembers], [Field | MoreFields], Result).
 
-jsonaccess([[Attribute , Punti , Value] | MoreMembers], [Field | MoreFields], Result) :-
+jsonaccess([[Attribute , ':' , Value] | MoreMembers], [Field | MoreFields], Result) :-
     Attribute == Field,
-    jsonaccess([MoreMembers], [MoreFields], [[Field, Punti, Value]]).
+    jsonaccess([MoreMembers], [MoreFields], [[Field, ':', Value]]).
 
 */
 
