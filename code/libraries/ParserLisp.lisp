e676a11f46e0078f8b26e7e1985b9b93cdd20ed1
Blekke <83006362+Blekke2801@users.noreply.github.com>
2023-02-24 19:23:55 +0100

    finire unify e scrivere il resto

--- code/libraries/ParserLisp.lisp
+++ code/libraries/ParserLisp.lisp
@@ -1,80 +1,101 @@
-(defun trim (n &rest list)
-  (cond ((null list) nil)
-    ((zerop n) list)
-    (T (cons (trim (1- n) (cdr list))))))
+(defun trim (n list)
+  (cond 
+    (null list) 
+      nil
+    (zerop n)  
+      list
+    T 
+      (cons (trim (1- n) (rest list)))))
 
-(defun subobj (&rest list)
-  (cond ((null list) nil)
-    ((char-equal (car list) #\{)
-      (cons (cons (cons (car list) (subobj (cdr list)))) (subobj (trim (list-length (subobj (cdr list)) (cdr list))))))
-    ((char-equal (car list) #\[)
-      (cons (cons (cons (car list) (subarray (cdr list)))) (subobj (trim (list-length (subarray (cdr list)) (cdr list))))))
-    ((char-equal (car list) #\}) #\})
-    (T 
-      (cons (car list) (subobj (cdr list))))))
+(defun subobj (list)
+  (cond 
+      (null list) 
+        nil
+      (char-equal (first list) #\{)
+        (cons (cons (cons (first list) (subobj (rest list)))) (subobj (trim (list-length (subobj (rest list)) (rest list)))))
+      (char-equal (first list) #\[)
+        (cons (cons (cons (first list) (subarray (rest list)))) (subobj (trim (list-length (subarray (rest list)) (rest list)))))
+      (char-equal (first list) #\}) 
+        #\}
+      T 
+        (cons (first list) (subobj (rest list)))))
 
-(defun subarray (&rest list)
+(defun subarray (list)
   (cond ((null list) nil)
-    ((char-equal (car list) #\[)
-      (cons (cons (car list) (subarray (cdr list))) (subarray (trim (list-length (subarray (cdr list)) (cdr list))))))
-    ((char-equal (car list) #\{)
-      (cons (cons (car list) (subobj (cdr list))) (subarray (trim (list-length (subobj (cdr list)) (cdr list))))))
-    ((char-equal (car list) #\]) #\})
-    (T (cons (car list) (subarray (cdr list))))))
+    ((char-equal (first list) #\[)
+      (cons (cons (first list) (subarray (rest list))) (subarray (trim (list-length (subarray (rest list)) (rest list))))))
+    ((char-equal (first list) #\{)
+      (cons (cons (first list) (subobj (rest list))) (subarray (trim (list-length (subobj (rest list)) (rest list))))))
+    ((char-equal (first list) #\]) #\})
+    (T (cons (first list) (subarray (rest list))))))
 
-(defun unify (&rest list)
-  (cond ((null list) nil)
-  ((char-equal (car list) #\")
-    (cons (cons (unify-quotes (cdr list))) (unify (trim (list-length (unify-quotes (cdr list)) (cdr list))))))
-  ((numberp (car list)) 
-    (cons (cons (car list) (unify-numbers (cdr list))) (unify (trim (list-length (unify-numbers (cdr list)) (cdr list))))))
-  (T (cons (car list) (unify (cdr list))))))
+(defun unify (String)
+  (cond 
+    (stringp String)
+      (unify (coerce String 'list))
+    (null String) 
+      nil
+    (char-equal (first String) #\")
+      (list (coerce (unify-quotes (rest String)) 'string) (unify (trim (+ (list-length (unify-quotes (rest String))) 1) (rest String))))
+    (numberp (first String)) 
+      (list (first String) (coerce (unify-numbers (rest String)) 'float) (unify (trim (list-length (unify-numbers (rest String))) (rest String))))
+    T 
+      (cons (first String) (unify (rest String)))))
 
-(defun unify-quotes (&rest list)
-  (cond ((null list) nil)
-  ((char-equal (car list) #\") nil)
-  (T (cons (car list) (unify-quotes (cdr list))))))
+(defun unify-quotes (list)
+  (cond 
+    (null list) 
+      nil
+    (char-equal (first list) #\") 
+      nil
+    T 
+      (cons (first list) (unify-quotes (rest list)))))
 
-(defun unify-numbers (&rest list)
-    (cond ((null list) nil)
-    (or (numberp (car list)) (char-equal (car list) #\.) (char-equal (car list) #\E))
-      (cons (car list) (unify-numbers (cdr list)))
-    (T nil)))
+(defun unify-numbers (list)
+    (cond 
+      (null list) 
+        nil
+      (or (numberp (first list)) (char-equal (first list) #\.) (char-equal (first list) #\E))
+        (cons (first list) (unify-numbers (rest list)))
+      T 
+        nil))
 
 (defun jsonparse (JSON)
   (cond 
     (stringp JSON)
-      trasformala in lista e se ha '{ all'inizio concatena jsonobj
-    (stringp JSON)
-      trasformala in lista e se ha '[ all'inizio concatena jsonarray
-    (cond 
-      (and (char-equal (first JSON) #\{) (stringp (second JSON)) (char-equal (third JSON) #\:))
+      (let (x (remove #\linefeed (remove #\Tab (remove #\Space (unify JSON)))))
         (cond 
-          (and (or (stringp (fourth JSON)) (numberp (fourth JSON))) (char-equal (fifth JSON) #\,))
-            (cons (cons (second JSON) (fourth JSON)) (jsonparse (cons #\{ (trim 5 JSON))))
-          (char-equal (fourth JSON) #\{)
-            (cons (cons (second JSON) (jsonparse (subobj (cdr (third JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subobj (cdr (third JSON)))) 1) JSON)))))
-          (char-equal (fourth JSON) #\[)
-            (cons (cons (second JSON) (jsonparse (subarray (cdr (third JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subarray (cdr (third JSON)))) 1) JSON)))))
-          (char-equal (second JSON) #\})
-            nil
+          (char-equal (first x) #\{)
+            (cons "jsonobj" (jsonparse x))
+          (char-equal (first x) #\[)
+            (cons "jsonarray" (jsonparse x))
           T
-            (error 'stringa json non valida))
-      (char-equal (first JSON) #\[)
-        (cond 
-          (or (stringp (second JSON)) (numberp (second JSON)))
-            (cons (second JSON) (jsonparse (cons #\[ (trim 3 JSON))))
-          (char-equal (second JSON) #\{)
-            (cons (jsonparse (subobj (cdr (second JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subarray (cdr (third JSON)))) 1) JSON))))
-          (char-equal (second JSON) #\[)
-            (cons (jsonparse (subarray (cdr (second JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subobj (cdr (third JSON)))) 1) JSON))))
-          (char-equal (second JSON) #\])
-            nil
-          T
-            (error 'stringa json non valida))
-      T
-        (error 'stringa json non valida))
+            (error "stringa json non valida")))
+    (and (char-equal (first JSON) #\{) (stringp (second JSON)) (char-equal (third JSON) #\:))
+      (cond 
+        (and (or (stringp (fourth JSON)) (numberp (fourth JSON))) (char-equal (fifth JSON) #\,))
+          (cons (cons (second JSON) (fourth JSON)) (jsonparse (cons #\{ (trim 5 JSON))))
+        (char-equal (fourth JSON) #\{)
+          (cons (cons (second JSON) (jsonparse (subobj (rest (third JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subobj (rest (third JSON)))) 1) JSON)))))
+        (char-equal (fourth JSON) #\[)
+          (cons (cons (second JSON) (jsonparse (subarray (rest (third JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subarray (rest (third JSON)))) 1) JSON)))))
+        (char-equal (second JSON) #\})
+          nil
+        T
+          (error "stringa json non valida"))
+    (char-equal (first JSON) #\[)
+      (cond 
+        (or (stringp (second JSON)) (numberp (second JSON)))
+          (cons (second JSON) (jsonparse (cons #\[ (trim 3 JSON))))
+        (char-equal (second JSON) #\{)
+          (cons (jsonparse (subobj (rest (second JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subarray (rest (third JSON)))) 1) JSON))))
+        (char-equal (second JSON) #\[)
+          (cons (jsonparse (subarray (rest (second JSON)))) (jsonparse (cons #\{ (trim (+ (list-length (subobj (rest (third JSON)))) 1) JSON))))
+        (char-equal (second JSON) #\])
+          nil
+        T
+          (error "stringa json non valida"))
   (null JSON)
     nil
   T
-    (error 'stringa json non valida)))
+    (error "stringa json non valida")))
